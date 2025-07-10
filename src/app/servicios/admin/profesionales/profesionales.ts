5c4a9c320d0b9c9ff7856b0f6d8efdb6e12c50d3
breyner <tejadabreyner@gmail.com>
2025-07-10 10:43:30 -0500

    prueba

--- src/app/servicios/admin/profesionales/profesionales.ts
+++ src/app/servicios/admin/profesionales/profesionales.ts
@@ -13,7 +13,7 @@ export interface Profesional {
   providedIn: 'root'
 })
 export class ProfesionalService {
-  private baseUrl = 'https://lunalimpia.onrender.com/profesional/';
+  private baseUrl = 'http://127.0.0.1:8000/profesional/';
   private cache: Profesional[] | null = null;
 
   constructor(private http: HttpClient) {}
